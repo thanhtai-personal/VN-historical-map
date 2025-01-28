7ce3c1bec2ebc6b8ae5df15d411d92fdf1ef6082
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2025-01-28 18:04:26 +0700

    Update setBaseLayerMap.ts

--- src/utils/setBaseLayerMap.ts
+++ src/utils/setBaseLayerMap.ts
@@ -4,11 +4,11 @@ export const setBaseLayer = (map: Map
   , titleLayer: string = "https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
   , options: any = {}
 ) => {
-  
+
   L.tileLayer(titleLayer, {
-  maxZoom: 12,
-  minZoom: 6,
-  attribution: "©taitran.dev",
-  ...options
-}).addTo(map);
+    maxZoom: 12,
+    minZoom: 6,
+    attribution: "©taitran.dev",
+    ...options
+  }).addTo(map);
 }
